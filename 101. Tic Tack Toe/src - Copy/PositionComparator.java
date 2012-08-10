10f4a09eca859211acbd091ccd2bda0ba6a578c8
rafi-kamal <rafikamal93@gmail.com>
2012-08-10 22:47:17 +0600

    Copy of /src deleted

--- 101. Tic Tack Toe/src - Copy/PositionComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class PositionComparator implements Comparator<PositionPriority>
-{
-	public int compare(PositionPriority a, PositionPriority b)
-	{
-		return b.priority - a.priority;
-	}
-}
